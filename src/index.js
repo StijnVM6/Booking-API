365b5e41a6cd1e8fc96451006221f4e7de0f67ee
StijnVM6 <stijn.vm@hotmail.com>
2024-09-18 14:26:07 +0200

    add: sentry.io

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+
 import loginRouter from "./routes/login/login.js";
 import usersRouter from "./routes/users/users.js";
 import amenitiesRouter from "./routes/amenities/amenities.js";
@@ -6,11 +7,23 @@ import hostsRouter from "./routes/hosts/hosts.js";
 import propertiesRouter from "./routes/properties/properties.js";
 import bookingsRouter from "./routes/bookings/bookings.js";
 import reviewsRouter from "./routes/reviews/reviews.js";
+
 import "dotenv/config";
 import logger from "./middleware/logger.js";
+// import * as Sentry from "@sentry/node";
+// import { nodeProfilingIntegration } from "@sentry/profiling-node";
+
+
 import customErrorHandler from "./middleware/customErrorHandler.js";
 
 const app = express();
+
+/*
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+});
+*/
 
 app.use(express.json());
 
@@ -24,6 +37,8 @@ app.use("/properties", propertiesRouter);
 app.use("/bookings", bookingsRouter);
 app.use("/reviews", reviewsRouter);
 
+// app.use(Sentry.Handlers.customErrorHandler());
+// Sentry.setupExpressErrorHandler(app);
 app.use(customErrorHandler);
 
 app.get("/", (req, res) => {
