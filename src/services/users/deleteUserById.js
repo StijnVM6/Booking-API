55b1ab1e3f30fb12d48d3032ffa9b4ca0dfb77af
StijnVM6 <stijn.vm@hotmail.com>
2024-09-18 13:45:57 +0200

    fix: schema relations delete & update behaviour

--- src/services/users/deleteUserById.js
+++ src/services/users/deleteUserById.js
@@ -1,8 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 import notFoundError from "../../errors/notFoundError.js";
+import getUserById from "./getUserById.js";
 
 const deleteUserById = async (id) => {
     const primsa = new PrismaClient();
+
+    // await getUserById(id);
+
+    // const updatedReviews = await primsa.review.updateMany({
+    //     where: { userId: id },
+    //     data: { userId: null }
+    // });
+    // if (updatedReviews.count <= 0) {
+    //     console.log(`No reviews affected.`);
+    // } else console.log(`Updated reviews with userId ${id} to null.`);
 
     const user = await primsa.user.deleteMany({
         where: { id: id }
